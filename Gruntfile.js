5bc21f6eb14a29b83a6ca7bc751aff7c11550dfd
dbreuer83 <dbreuer83@gmail.com>
2015-03-24 07:38:28 +0000

    Grunt build update angular JS minified

--- Gruntfile.js
+++ Gruntfile.js
@@ -227,16 +227,16 @@ module.exports = function (grunt) {
         },
 
         // Renames files for browser caching purposes
-        //filerev: {
-        //    dist: {
-        //        src: [
-        //            '<%= yeoman.dist %>/scripts/{,*/}*.js',
-        //            '<%= yeoman.dist %>/styles/{,*/}*.css',
-        //            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-        //            '<%= yeoman.dist %>/styles/fonts/*'
-        //        ]
-        //    }
-        //},
+        filerev: {
+            dist: {
+                src: [
+                    '<%= yeoman.dist %>/scripts/{,*/}*.js',
+                    '<%= yeoman.dist %>/styles/{,*/}*.css',
+                    '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                    '<%= yeoman.dist %>/styles/fonts/*'
+                ]
+            }
+        },
 
         // Reads HTML for usemin blocks to enable smart builds that automatically
         // concat, minify and revision files. Creates configurations in memory so
@@ -448,19 +448,19 @@ module.exports = function (grunt) {
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
-        //'concat',
+        'concat',
         'copy',
         //'cdnify',
         'cssmin',
         'uglify',
-        //'filerev',
+        'filerev',
         'usemin',
-        'htmlmin',
+        //'htmlmin',
         'watch'
     ]);
 
     grunt.registerTask('default', [
-        //'newer:jshint',
+        'newer:jshint',
         'test',
         'build'
     ]);
